727b3394c4551db9e285785ff3bc446490417fd2
Dmitrievich Vyacheslav <ittkirsan@gmail.com>
2022-01-10 15:23:44 +0300

    Update task1_8

--- Linkedlist/task1_8.py
+++ Linkedlist/task1_8.py
@@ -1,14 +1,15 @@
+from typing import Any
 from task1 import LinkedList, Node
 
 
-def SumEqualLinkedList(LList1, LList2):
-    LList_result = LinkedList()
+def SumEqualLinkedList(LList1: LinkedList, LList2: LinkedList):
+    LList_result: LinkedList = LinkedList()
     if LList1.len() == LList2.len():
-        node1 = LList1.head  # встаем вначало списка 1
-        node2 = LList2.head  # встаем вначало списка 2
+        node1: Node = LList1.head  # встаем вначало списка 1
+        node2: Node = LList2.head  # встаем вначало списка 2
         while node1 is not None:
-            sum = node1.value + node2.value  # сумируем значение узлов
+            sum: int = node1.value + node2.value  # сумируем значение узлов
             # создаем узел и добавляем его в список
             LList_result.add_in_tail(Node(sum))
             node1, node2 = node1.next, node2.next
-        return LList_result
+    return LList_result
