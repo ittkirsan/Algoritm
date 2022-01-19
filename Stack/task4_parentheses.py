198d9bae558ca6f3fea6338e6526e71a3ec40510
Dmitrievich Vyacheslav <ittkirsan@gmail.com>
2022-01-19 14:52:13 +0300

    Added task4_5

--- Stack/task4_parentheses.py
+++ Stack/task4_parentheses.py
@@ -1,4 +1,4 @@
-from inspect import stack
+'''Функция для определения сбалансированности скобочного рядаю'''
 from task4_stack_tail import *
 
 
@@ -7,7 +7,7 @@ def parentheses(string: str):
     for i in string:
         if i in '(':
             stack.push(i)
-        else:
+        else:  # удаляем пару
             if stack.pop() is None:
                 return False
     return stack.size() == 0
